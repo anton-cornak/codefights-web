911f01dffe222d0e9c0bb6838ed7e1d0bbebaa44
Lucia Bodnarova <lucia.bodnarova@kosickaakademia.sk>
2023-05-08 13:14:17 +0200

    password toggle button added

--- front-end/pages/signin.tsx
+++ front-end/pages/signin.tsx
@@ -2,11 +2,16 @@ import { signIn } from "next-auth/react";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
 import BoxShadowImage from "components/BoxShadowImage";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
 const SignInPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const router = useRouter();
   const callbackUrl = decodeURI((router.query?.callbackUrl as string) ?? "/");
   const handleSubmit = async (
@@ -26,54 +31,84 @@ const SignInPage = () => {
       router.push(callbackUrl);
     }
   };
+
   return (
-    <div>
-        <div className="flex">
-               {/* lava cast obrazovky */}
-                <div className="flex-1"> 
-            <h1 className="text-5xl text-left ml-48 mt-28 text-gray-700">LOGIN</h1>
-          <form className="card-body w-96 max-w-md mx-auto mt-8 ml-24" onSubmit={handleSubmit}>
-            {!!error && <p className="text-error">ERROR: {error}</p>}
+  <div>
+    <div className="md:flex">
+      <div className="p-8">
+        <div className="mt-4 mb-0 uppercase tracking-wide text-3xl text-grey-700 font-semibold text-center">
+          login
+        </div>
+        <form
+          className="block mt-20 text-lg leading-tight font-medium text-black hover:text-grey-700"
+          onSubmit={handleSubmit}
+        >
+          {!!error && <p className="text-error">ERROR: {error}</p>}
+          <div className="relative">
             <input
               type="text"
-              className="input input-bordered mt-1 px-3 py-2 border rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-gray-600"
+              className="ml-16 mt-0 px-3 py-2 border rounded-lg w-3/4 focus:outline-none focus:ring-2 focus:ring-gray-600"
               placeholder="email"
               value={email}
               onChange={(e) => {
                 setEmail(e.target.value);
               }}
             />
+          </div>
+          <div className="relative">
             <input
-              type="password"
-              className="input input-bordered mt-1 px-3 py-2 border rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-gray-600"
+              type={showPassword ? "text" : "password"}
+              className="ml-16 mt-4 px-3 py-2 border rounded-lg w-3/4 focus:outline-none focus:ring-2 focus:ring-gray-600"
               placeholder="password"
               value={password}
               onChange={(e) => {
                 setPassword(e.target.value);
               }}
             />
-            <button className="btn" type="submit">
-              Sign In
-            </button>
-          </form>
-        </div>
+            {showPassword ? (
+              <FontAwesomeIcon
+                icon={faEyeSlash}
+                onClick={() => setShowPassword(!showPassword)}
+                style={{
+                  position: "absolute",
+                  top: "50%",
+                  right: "30px",
+                  transform: "translateY(-50%)",
+                  cursor: "pointer",
+                }}
+              />
+            ) : (
+              <FontAwesomeIcon
+                icon={faEye}
+                onClick={() => setShowPassword(!showPassword)}
+                style={{
+                  position: "absolute",
+                  top: "60%",
+                  right: "10%",
+                  transform: "translateY(-50%)",
+                  cursor: "pointer",
+                }}
+              />
+            )}
+          </div>
 
-                  {/* prava cast obrazovky */}
-                <div className="w-1/3 mt-10 mr-48"> 
-                  <div className="relative">
-                    
-<BoxShadowImage
-        alt="Example Image"
-        src="loginImage.jpg"
-        width={350}
-        height={350
-        }
-      />                           
-    </div>
-  </div>
+          <br />
+          <button className="ml-60 mt-4 uppercase px-4 py-2 text-sm font-medium text-white bg-custom_light_grey rounded-sm hover:bg-custom_green focus:outline-none focus:ring-2 focus:ring-custom_green" type="submit">
+            Login
+          </button>
+        </form>
+      </div>
+      <div className="md:shrink-0 mr-16 mt-10 ml-16">
+        <BoxShadowImage
+          alt="Example Image"
+          src="loginImage.jpg"
+          width={350}
+          height={350}
+        />
       </div>
     </div>
-  );
-};
+  </div>
+);
+}
 
 export default SignInPage;
