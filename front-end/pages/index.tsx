25719498303b0ddc81588e037b6822b2d00c0a70
Jannosek <jan.nosek@kosickaakademia.sk>
2023-05-11 15:07:00 +0200

    design

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-import Navbar from "@/components/Navbar";
-
-import '../pages/'
-export default function Home() {
-    return (
-      <>
-        <Navbar />
-        <div className=' bg-black relative h-screen overflow-x-hidden'>
-
-        <div className='container flex flex-col justify-start items-center w-full h-fit'></div>
-
-        <div className="inline-flex items-center justify-center w-full">
-                <hr className="h-px w-[85rem] bg-white border-0 dark:bg-white "></hr>
-            </div>
-
-            </div>
-        </>)
-=======
 /* eslint-disable @next/next/no-page-custom-font */
 /* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
@@ -156,5 +137,4 @@ export default function Home() {
 
     </div>
   );
->>>>>>> ad1aa7e (design)
 }
