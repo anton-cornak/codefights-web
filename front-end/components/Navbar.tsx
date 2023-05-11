d7c7dbec49027aaf4f6c1dd32d9bef3140a8a8b8
Jannosek <jan.nosek@kosickaakademia.sk>
2023-05-11 16:12:53 +0200

    daco

--- front-end/components/Navbar.tsx
+++ front-end/components/Navbar.tsx
@@ -1,52 +1,3 @@
-<<<<<<< HEAD
-import Link from 'next/link'
-import { buttonVariants } from './Button'
-import SignInButton from './SignInButton'
-import BoxShadowImage from "./BoxShadowImage";
-
-
-const Navbar = () => {
-    
-
-    return (
-        <div className='sticky w-full backdrop-blur-sm bg-white/75 dark:bg-black z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-600 shadow-sm flex items-center justify-center '>
-            <div className='container max-width-7xl mx-auto w-full flex justify-center items-center'>
-                <Link href='/' className={buttonVariants({ variant: 'link' })}>
-                    HOME
-                </Link>
-
-                <Link href='/events' className={buttonVariants({ variant: 'link' })}>
-                    EVENTS
-                </Link>
-
-                <Link href='/leaderboard' className={buttonVariants({ variant: 'link' })}>
-                    LEADERBOARD
-                </Link>
-
-            </div>
-
-
-            
-
-            <div className='container max-width-7xl mx-auto w-full flex justify-center items-center'>
-
- 
-                <div className='hidden md:flex gap-4'>
-                            <Link className={buttonVariants({ variant: 'default' })}
-                                href='/register'>
-                                REGISTER
-                            </Link>
-                       
-                    <SignInButton />
-
-                </div>
-            </div>
-        </div>
-    )
-}
-
-export default Navbar;
-=======
 import { useState } from "react";
 import Link from "next/link";
 
@@ -74,4 +25,3 @@ export default function Navbar() {
     </>
   );
 }
->>>>>>> ad1aa7e (design)
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,45 +1,8 @@
-<<<<<<< HEAD
-const { colors } = require('tailwindcss/colors');
-const { fontFamily } = require('tailwindcss/defaultTheme');
-
-/** @type {import('tailwindcss').Config} */
-=======
->>>>>>> ad1aa7e (design)
 module.exports = {
   content: [
     "./pages/**/*.{js,jsx,ts,tsx,css}",
     "./components/**/*.{js,jsx,ts,tsx,css}",
   ],
-<<<<<<< HEAD
-  daisyui: {
-    themes: ["light"],
-  },
-  theme: {
-    container: {
-      center: true,
-      padding: '1.5rem',
-      screens: {
-        '2xl': '1360px'
-      }
-    },
-    extend: {
-      fontFamily: {
-        'bruno': ['"Bruno Ace SC"', 'sans-serif'],
-      },
-      colors: {
-        ...colors,
-        'light-red': '#D50641',
-        'dark-red': '#EB0F32',
-        'green': '#97A21E',
-        'dark-green': '#4c5211',
-        "light-green": '#CFDE38',
-        'color1':'#BDB6D0',
-        'color2':'#97A21E',
-        'bgBlack':'#000000',
-     
-
-      },
-=======
   theme: {
     extend: {
       colors: {
@@ -55,7 +18,6 @@ module.exports = {
         '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
       }
       
->>>>>>> ad1aa7e (design)
     },
   },
   plugins: [
@@ -65,9 +27,5 @@ module.exports = {
     require("@tailwindcss/line-clamp"),
     require("daisyui"),
     require("prettier-plugin-tailwindcss"),
-<<<<<<< HEAD
-    require('tailwindcss-animate')
-=======
->>>>>>> ad1aa7e (design)
   ],
 };
