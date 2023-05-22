dfd0e7620a50790062eb2dfc521ff0799d0e6c85
Jannosek <jan.nosek@kosickaakademia.sk>
2023-05-22 21:38:19 +0200

    Revert "changed frontend"
    This reverts commit 0767d0ba5123c2c43f918d7f7fe77722d49b3027.

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -1,17 +1,15 @@
 import '../pages/'
 export default function Home() {
-  return (
-    <>
-      <div className=' bg-black relative h-screen overflow-x-hidden'>
+    return (
+      <>
+        <div className=' bg-black relative h-screen overflow-x-hidden'>
 
         <div className='container flex flex-col justify-start items-center w-full h-fit'></div>
 
         <div className="inline-flex items-center justify-center w-full">
-          <hr className="h-px w-[85rem] bg-white border-0 dark:bg-white "></hr>
-        </div>
-        <div className="text-9xl absolute mt-24 top-96 left-40">
-          <h1>WARS</h1>
-        </div>[]
-      </div>
-    </>)
+                <hr className="h-px w-[85rem] bg-white border-0 dark:bg-white "></hr>
+            </div>
+
+            </div>
+        </>)
 }
