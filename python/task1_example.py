cc41d364386cf4790f3ce056ecc036f2a0c11164
danielvajda <daniel.vajda@kosickaakademia.sk>
2023-05-23 11:32:21 +0200

    updated

--- python/task1_example.py
+++ python/task1_example.py
@@ -10,12 +10,21 @@ Return only letters: if string contains other symbols, ignore them.
 This is the first example challenge. Obviously, this needs to be updated heavily for the whole code-fights service concept.
 Consider the following code to be only an example on how to evaluate the code
 """
+import unittest
 import random
 import string
 import time
 import timeout_decorator
 
-def correct_solution2(text: str) -> str:
+
+def alphabet_solution(text: str) -> str:
+    """
+    Placeholder - this is user input
+    """
+    return text
+
+
+def correct_solution(text: str) -> str:
     return " ".join(
         str(string.ascii_lowercase.index(letter.lower()) + 1)
         for letter in text
@@ -23,75 +32,87 @@ def correct_solution2(text: str) -> str:
     )
 
 
-def test_predefined(user_submitted_func):
+class AlphabetTestCase(unittest.TestCase):
     """
-    Test edge cases
+    Unit tests
     """
-    exec(user_submitted_func)
-    testcases = [
-        "abcdefghijklmnopqrstuvwxyz",
-        "         a      ",
-        "./}!@*&#@&^$",
-        "./*&#}!@as@2dz&^$",
-        "The sunset sets at twelve o' clock.",
-        "The narwhal bacons at midnight.",
-        "Visma Labs Slovakia",
-        "I love code fights!",
-        "abcdefghijklmnopqrstuvwxyz",
-        "1230984192381029",
-    ]
-    for testcase in testcases:
-        actual = correct_solution(testcase)
-        expected = correct_solution2(testcase)
-        assert actual == expected, f'\nfailed test case "{testcase}": expected "{expected}", but got "{actual}".'
+    maxDiff = None  # Set maxDiff to None to display the entire diff
 
-    return "test_predefined_success"
-
-
-@timeout_decorator.timeout(5)
-def test_random_lowercase(user_submitted_func):
-    """
-    Test randomly generated strings - lowercase
-    """
-    for _ in range(10):
-        testcase = "".join(random.choices(string.ascii_lowercase, k=300))
-        actual = correct_solution(testcase)
-        expected = correct_solution2(testcase)
-        assert actual == expected, f'\nfailed test case "{testcase}": expected "{expected}", but got "{actual}".'
-
-    return "random_lowercase_success"
-
-@timeout_decorator.timeout(5)
-def test_random_uppercase(user_submitted_func):
-    """
-    Test randomly generated strings - uppercase
-    """
-    for _ in range(10):
-        testcase = "".join(random.choices(string.ascii_uppercase, k=300))
-        actual = correct_solution(testcase)
-        expected = correct_solution2(testcase)
-        assert actual == expected, f'\nfailed test case "{testcase}": expected "{expected}", but got "{actual}".'
-
-    return "random_uppercase_success"
-
-@timeout_decorator.timeout(20)
-def test_performance(user_submitted_func):
-    """
-    Performance test
-    """
-    times = []
-
-    for _ in range(10):
-        st = time.process_time()
-        for _ in range(100):
-            correct_solution(
-                "".join(random.choices(string.ascii_uppercase, k=10000))
+    def test_predefined(self):
+        """
+        Test edge cases
+        """
+        testcases = [
+            "abcdefghijklmnopqrstuvwxyz",
+            "         a      ",
+            "./}!@*&#@&^$",
+            "./*&#}!@as@2dz&^$",
+            "The sunset sets at twelve o' clock.",
+            "The narwhal bacons at midnight.",
+            "Visma Labs Slovakia",
+            "I love code fights!",
+            "abcdefghijklmnopqrstuvwxyz",
+            "1230984192381029",
+        ]
+        for testcase in testcases:
+            actual = alphabet_solution(testcase)
+            expected = correct_solution(testcase)
+            self.assertEqual(
+                actual,
+                expected,
+                f'\nfailed test case "{testcase}": expected "{expected}", but got "{actual}".',
             )
-        et = time.process_time()
-        times.append(et - st)
 
-    print(
-        f"\nYour average execution time is: {sum(times)/len(times)}s. Benchmark: 0.6s."
-    )
+    @timeout_decorator.timeout(5)
+    def test_random_lowercase(self):
+        """
+        Test randomly generated strings - lowercase
+        """
+        for _ in range(10):
+            testcase = "".join(random.choices(string.ascii_lowercase, k=300))
+            actual = alphabet_solution(testcase)
+            expected = correct_solution(testcase)
+            self.assertEqual(
+                actual,
+                expected,
+                f'\nfailed test case "{testcase}": expected "{expected}", but got "{actual}".',
+            )
 
-    return "test_performance_success"
+    @timeout_decorator.timeout(5)
+    def test_random_uppercase(self):
+        """
+        Test randomly generated strings - uppercase
+        """
+        for _ in range(10):
+            testcase = "".join(random.choices(string.ascii_uppercase, k=300))
+            actual = alphabet_solution(testcase)
+            expected = correct_solution(testcase)
+            self.assertEqual(
+                actual,
+                expected,
+                f'\nfailed test case "{testcase}": expected "{expected}", but got "{actual}".',
+            )
+
+    @timeout_decorator.timeout(20)
+    def test_performance(self):
+        """
+        Performance test
+        """
+        times = []
+
+        for _ in range(10):
+            st = time.process_time()
+            for _ in range(100):
+                alphabet_solution(
+                    "".join(random.choices(string.ascii_uppercase, k=10000))
+                )
+            et = time.process_time()
+            times.append(et - st)
+
+        print(
+            f"\nYour average execution time is: {sum(times)/len(times)}s. Benchmark: 0.6s."
+        )
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=0)
