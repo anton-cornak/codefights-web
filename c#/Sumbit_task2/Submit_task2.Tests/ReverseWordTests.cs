4441f82aa487dbbe9825fa84e64654532b45704e
Jaroslav Krajňák <jaroslav.krajnak@kosickaakademia.sk>
2023-05-30 19:43:44 +0200

    my first csharp push

---
+++ c#/Sumbit_task2/Submit_task2.Tests/ReverseWordTests.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Emit;
+using System.Reflection;
+
+[TestFixture]
+public class ReverseWordTests
+{
+    private string tempFilePath;
+    private List<Tuple<string, bool>> testResults = new List<Tuple<string, bool>>();
+
+    public ReverseWordTests(string tempFilePath)
+    {
+        this.tempFilePath = tempFilePath;
+    }
+
+    private string GetMethodFromTempFile()
+    {
+        string methodContent = File.ReadAllText(tempFilePath);
+        return methodContent;
+    }
+
+    public void RunTests()
+    {
+        ReverseWord_WithValidInput_ReturnsReversedWord();
+        ReverseWord_WithEmptyInput_ReturnsEmptyString();
+        
+    }
+
+    public List<Tuple<string, bool>> GetResults()
+    {
+        return testResults;
+    }
+
+    [Test]
+    public void ReverseWord_WithValidInput_ReturnsReversedWord()
+    {
+        // Arrange
+        string word = "Hello";
+        string expected = "olleH";
+
+        // Read the method content from the temporary file
+        string methodContent = GetMethodFromTempFile();
+
+        // Create a dynamic assembly
+        Assembly assembly = BuildAssembly(methodContent);
+
+        // Get the ReverseWord method from the assembly
+        MethodInfo method = assembly.GetType("TempNamespace.TempClass").GetMethod("ReverseWord");
+
+        // Act
+        string result = (string)method.Invoke(null, new object[] { word });
+
+        // Assert
+        bool passed = result == expected;
+        string testName = "ReverseWord_WithValidInput_ReturnsReversedWord";
+        Tuple<string, bool> testResult = Tuple.Create(testName, passed);
+        testResults.Add(testResult);
+    }
+    [Test]
+    public void ReverseWord_WithEmptyInput_ReturnsEmptyString()
+    {
+        // Arrange
+        string word = string.Empty;
+        string expected = string.Empty;
+
+        // Read the method content from the temporary file
+        string methodContent = GetMethodFromTempFile();
+
+        // Create a dynamic assembly
+        Assembly assembly = BuildAssembly(methodContent);
+
+        // Get the ReverseWord method from the assembly
+        MethodInfo method = assembly.GetType("TempNamespace.TempClass").GetMethod("ReverseWord");
+
+        // Act
+        string result = (string)method.Invoke(null, new object[] { word });
+
+        // Assert
+        bool passed = result == expected;
+        string testName = "ReverseWord_WithEmptyInput_ReturnsEmptyString";
+        Tuple<string, bool> testResult = Tuple.Create(testName, passed);
+        testResults.Add(testResult);
+    }
+    
+    private Assembly BuildAssembly(string methodContent)
+    {
+        // Define the class and method
+        string className = "TempNamespace.TempClass";
+        string code = $@"
+        using System;
+        namespace TempNamespace
+        {{
+            public static class TempClass
+            {{
+                {methodContent}
+            }}
+        }}";
+
+        // Create syntax tree from code
+        SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(code);
+
+        // Define compilation options
+        CSharpCompilationOptions compilationOptions = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary);
+
+        // Create compilation
+        CSharpCompilation compilation = CSharpCompilation.Create("TempAssembly")
+            .WithOptions(compilationOptions)
+            .AddReferences(MetadataReference.CreateFromFile(typeof(object).Assembly.Location))
+            .AddSyntaxTrees(syntaxTree);
+
+        using (MemoryStream ms = new MemoryStream())
+        {
+            // Emit the assembly to a memory stream
+            EmitResult emitResult = compilation.Emit(ms);
+
+            if (!emitResult.Success)
+            {
+                // Handle compilation errors
+                IEnumerable<Diagnostic> failures = emitResult.Diagnostics.Where(diagnostic =>
+                    diagnostic.IsWarningAsError || diagnostic.Severity == DiagnosticSeverity.Error);
+
+                string errorMessage = string.Join(Environment.NewLine, failures.Select(diagnostic => diagnostic.ToString()));
+                Console.Error.WriteLine(errorMessage);
+
+                throw new Exception("Failed to compile the code.");
+            }
+
+            // Load the assembly from the memory stream
+            ms.Seek(0, SeekOrigin.Begin);
+            Assembly assembly = Assembly.Load(ms.ToArray());
+
+            return assembly;
+        }
+    }
+}
---
+++ c#/Sumbit_task2/Sumbit_task2/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Text;
+using SimpleHttp;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace Submit_task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Route.Add("/submit", (req, res, props) =>
+            {
+                using (StreamReader reader = new StreamReader(req.InputStream))
+                {
+                    string requestBody = reader.ReadToEnd();
+                    dynamic requestData = JsonConvert.DeserializeObject(requestBody);
+                    dynamic data = JObject.Parse(requestData.ToString());
+                    Console.WriteLine(data.code);
+
+                    // Getting code in base64
+                    string code2 = data.code;
+
+                    byte[] data2 = Convert.FromBase64String(code2);
+
+                    // Decoding from base64
+                    string decodedString = Encoding.UTF8.GetString(data2);
+                    Console.WriteLine(decodedString);
+
+                    // Create temp file
+                    string tempFilePath = Path.GetTempFileName();
+
+                    // Save encoded code to temporary file
+                    string tempCode = decodedString;
+                    File.WriteAllText(tempFilePath, tempCode);
+
+                    // Display the full path to the temporary file
+                    Console.WriteLine("Temporary file path: " + tempFilePath);
+
+                    // Read and print the content of the temporary file
+                    string fileContent = File.ReadAllText(tempFilePath);
+                    Console.WriteLine("Code in the temporary file:");
+                    Console.WriteLine(fileContent);
+
+                    // Execute the tests and store the results
+                    List<Tuple<string, bool>> testResults = ExecuteTests(tempFilePath);
+                    
+                    // Calculate the number of passed and failed tests
+                    int passedCount = testResults.Count(result => result.Item2);
+                    int failedCount = testResults.Count(result => !result.Item2);
+
+                    // Prepare the response content
+                    string responseContent = $"Passed: {passedCount}, Failed: {failedCount}";
+
+                    // Delete the temporary file
+                    File.Delete(tempFilePath);
+                    
+                    Console.WriteLine(JsonConvert.SerializeObject(testResults));
+
+                    // Send the test results as a response
+                    res.AsText(JsonConvert.SerializeObject(responseContent));
+
+                    Console.ReadLine();
+                }
+            }, "POST");
+
+
+            HttpServer.ListenAsync(
+                    1337,
+                    CancellationToken.None,
+                    Route.OnHttpRequestAsync
+                    )
+                    .Wait();
+        }
+
+        static List<Tuple<string, bool>> ExecuteTests(string tempFilePath)
+        {
+            ReverseWordTests reverseWordTests = new ReverseWordTests(tempFilePath);
+            reverseWordTests.RunTests();
+            return reverseWordTests.GetResults();
+        }
+    }
+}
