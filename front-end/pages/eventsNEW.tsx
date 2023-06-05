7ab82f3a3534933a96130b57d04f24f39ddd39a8
Lucia Bodnarova <lucia.bodnarova@kosickaakademia.sk>
2023-06-05 10:08:33 +0200

    isLoading added

--- front-end/pages/eventsNEW.tsx
+++ front-end/pages/eventsNEW.tsx
@@ -11,6 +11,8 @@ interface Event {
 
 const EventsPage: React.FC = () => {
     const [events, setEvents] = useState<Event[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+
 
     useEffect(() => {
         axios
@@ -23,9 +25,11 @@ const EventsPage: React.FC = () => {
                     photo: `data:image/jpeg;base64,${event.photo}`,
                 }));
                 setEvents(eventData);
+                setIsLoading(false);
             })
             .catch((error) => {
                 console.error('Error fetching events:', error);
+                setIsLoading(false);
             });
     }, []);
 
@@ -35,16 +39,20 @@ const EventsPage: React.FC = () => {
     <div className="bg-white dark:bg-black">
    <h1 className="text-5xl mt-120">Upcoming Events</h1>
             <hr className="border-t-2 border-black w-2/3" />
-            <div className="flex flex-wrap justify-center mt-8">
-                {events.map((event) => (
-                    <div key={event._id} className="w-1/2 lg:w-1/2 p-4">
-                        <h3 className='text-3xl'>{event.name}</h3>
-                        <p>{event.description}</p>
-                        <img src={event.photo} alt="Event" className="h-64 w-132" />
-                        <StartEventButton eventId={event._id} token={token} />
+
+            {isLoading ? (<div className="text-center mt-8 text-lg">Loading events...</div>)
+                : (
+                    <div className="flex flex-wrap justify-center mt-8">
+                        {events.map((event) => (
+                            <div key={event._id} className="w-1/2 lg:w-1/2 p-4">
+                                <h3 className='text-3xl'>{event.name}</h3>
+                                <p>{event.description}</p>
+                                <img src={event.photo} alt="Event" className="h-64 w-132" />
+                                <StartEventButton eventId={event._id} token={token} />
+                            </div>
+                        ))}
                     </div>
-                ))}
-            </div> 
+                )}
         </div>
     );
 }
